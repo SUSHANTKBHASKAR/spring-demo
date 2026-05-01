8f4d4f8ccb2ce9530fa0812e984053e6d174a08d
Sushant.k.bhaskar <sushantkbhaskar5@gmail.com>
2026-05-01 21:01:58 +0530

    spring-demo updated

--- src/main/java/com/sushant/App.java
+++ src/main/java/com/sushant/App.java
@@ -1,15 +1,16 @@
 package com.sushant;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
 
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(BeanDemo.class);
+        ApplicationContext container =
+                new ClassPathXmlApplicationContext("spring-config.xml");
+        BeanDemo bean = container.getBean(BeanDemo.class);
+        bean.add(4,5);
 
-        BeanDemo bean = context.getBean(BeanDemo.class);
-        bean.add(6, 3);
     }
 }
